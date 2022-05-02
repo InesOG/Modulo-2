291fb4d8730a74f853838a4a9146f46589ced550
InesOG <i.ontillera@gmail.com>
2022-05-02 23:43:02 +0100

    Initial commit

---
+++ Ejercicios/Ej_Dayana.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const superWave = document.querySelector(".super-wave");
+
+console.log(superWave);
+
+//const spanElement = document.querySelector('span');
+
+//spanElement.classList.add('destacada');
+
+superWave.classList.add('destacada');
+
+const nu = 90228;
+
+console.log(nu);
