e638c072494b519a20eb1dea5de0aaa92fbf0338
InesOG <i.ontillera@gmail.com>
2022-05-29 18:22:17 +0100

    Change background color triggered by scroll event and based on Y scroll

---
+++ Ejercicios/4 Eventos/Ejercicio5.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const divElement = document.querySelector('div');
+
+window.addEventListener('scroll', () => {
+    const last_known_scroll_position = window.scrollY;
+
+    if (divElement.classList.contains('background-red')) {
+        divElement.classList.remove('background-red');
+    };
+
+    if (divElement.classList.contains('background-green')) {
+        divElement.classList.remove('background-green');
+    };
+    
+    if (last_known_scroll_position < 250) {
+        divElement.classList.add('background-green');
+    } else {
+        divElement.classList.add('background-red');
+    } 
+    console.log(last_known_scroll_position);
+});
