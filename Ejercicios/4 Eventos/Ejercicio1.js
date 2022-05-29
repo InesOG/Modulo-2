fad4f16aa8a4d55ea240446b6c2dfa09c7d1ae07
InesOG <i.ontillera@gmail.com>
2022-05-29 13:04:14 +0100

    Exercise 1 and 2 of Chapter 4; events

---
+++ Ejercicios/4 Eventos/Ejercicio1.js
@@ -0,0 +1,21 @@
+"use strict"
+
+// elemento de HTML
+const buttonX = document.querySelector(".alert");
+
+const h1Element = document.querySelector('h1');
+// listener sobre el elemento, con tipo de evento y el código a ejecutarse
+buttonX.addEventListener("click", () => {
+  h1Element.innerHTML = "Mi primer click"; //código a ejecutarse
+});
+
+
+const buttonSubmit = document.querySelector('.name');
+
+const inputElement = document.querySelector('input');
+
+
+buttonSubmit.addEventListener('click', () => {
+    const nameSubmitted = inputElement.value;
+    console.log(`Hola ${nameSubmitted}`);
+});
