eae8e791f4ea1ea3ff9c724df41de1f4bca930b3
InesOG <i.ontillera@gmail.com>
2022-11-05 20:05:47 +0000

    Ejercicio 2 fotos de perros

---
+++ Ejercicios/10 Server Request/Example2.js
@@ -0,0 +1,10 @@
+function getDogPict() {
+    fetch("https://dog.ceo/api/breeds/image/random")
+        .then((response) => response.json())
+        .then((data) => {
+            const img = document.querySelector("img");
+            img.src = data.message;
+            img.alt = "Un perro";
+        });
+    }
+    document.querySelector(".js-dog").addEventListener("click", getDogPict);
