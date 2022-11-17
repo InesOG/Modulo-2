c207859a68fbf1e6aa4c676008f0db1d5e6b859b
InesOG <i.ontillera@gmail.com>
2022-11-17 22:31:27 +0000

    Exercise6 print list coming from a Promise

---
+++ Ejercicios/10 Server Request/Exercise6.js
@@ -0,0 +1,34 @@
+/*function getRandomBreed() {
+
+}
+
+function getBreedList() {
+    fetch('https://dog.ceo/api/breeds/list/all')
+        .then((breedListResponse) => breedListResponse.json())
+        .then((breedMessage) => breedMessage.message)
+}*/
+
+
+
+const dogsOl = document.getElementById("dogsList")
+
+fetch('https://dog.ceo/api/breeds/list/all')
+        .then((breedListResponse) => breedListResponse.json())
+        .then((breedMessage) => { 
+        const breedsmessage = breedMessage.message;
+        const breedslist = []
+        for (const [breed,variants] of Object.entries(breedsmessage)) {            
+            let dog = document.createElement("li");
+            
+            if (variants.length === 0) {
+                dog.innerText = `${breed}`;
+                dogsOl.appendChild(dog);
+            }; 
+
+            for (const variant of variants) {
+                dog.innerText = `${variant} ${breed}`;
+                dogsOl.appendChild(dog); 
+            };
+          
+        };
+    })
