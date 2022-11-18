e67b8e81dec6cfcae72081106df85d70709108b7
InesOG <i.ontillera@gmail.com>
2022-11-18 21:14:20 +0000

    Variant using debugger to explore variables

--- Ejercicios/10 Server Request/Exercise6.js
+++ Ejercicios/10 Server Request/Exercise6.js
@@ -32,3 +32,28 @@ fetch('https://dog.ceo/api/breeds/list/all')
           
         };
     })
+
+
+fetch('https://dog.ceo/api/breeds/list/all')
+    .then((breedListResponse) => breedListResponse.json())
+    .then((breedMessage) => { 
+    const breedsmessage = breedMessage.message;
+    const breedslist = []
+    for (const [breed,variants] of Object.entries(breedsmessage)) {            
+        let dog = document.createElement("li");
+        
+        if (variants.length === 0) {
+            breedslist.push(breed);
+        }; 
+
+        for (const variant of variants) {
+            breedslist.push(`${variant} ${breed}`);
+        };
+      
+    };
+
+    const numberOfBreeds = breedslist.length;
+
+    
+
+})
