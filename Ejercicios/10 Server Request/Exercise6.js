d88d99555ac4f41689b2b7e85445a25e2f15bb08
InesOG <i.ontillera@gmail.com>
2022-11-18 22:19:59 +0000

    Shows random image of randomnly selected breed

--- Ejercicios/10 Server Request/Exercise6.js
+++ Ejercicios/10 Server Request/Exercise6.js
@@ -6,7 +6,7 @@ function getBreedList() {
     fetch('https://dog.ceo/api/breeds/list/all')
         .then((breedListResponse) => breedListResponse.json())
         .then((breedMessage) => breedMessage.message)
-}*/
+}
 
 
 
@@ -32,28 +32,63 @@ fetch('https://dog.ceo/api/breeds/list/all')
           
         };
     })
+*/
+
+function getRandomBreedAndImage() {
+    fetch('https://dog.ceo/api/breeds/list/all')
+        .then((breedListResponse) => breedListResponse.json())
+        .then((breedMessage) => { 
+            const breedsmessage = breedMessage.message;
+            const breedslist = []
+            for (const [breed,variants] of Object.entries(breedsmessage)) {            
+                let dog = document.createElement("li");
+                
+                if (variants.length === 0) {
+                    breedslist.push(breed);
+                }; 
+
+                for (const variant of variants) {
+                    breedslist.push(`${variant} ${breed}`);
+                };
+            
+            };
+
+            const numberOfBreeds = breedslist.length;        
+            const bodyElement = document.querySelector('body');
+
+            fetch(`https://api.rand.fun/number/integer?max=${numberOfBreeds}`)
+                .then((rndResponse) => rndResponse.json())
+                .then((rndResult) => {
+                    const rndNumber = rndResult.result;
+                    console.log(rndNumber);
+                    const breed = breedslist[rndNumber]
+                    console.log(breed);         
+
+                    fetch(`https://dog.ceo/api/breed/${breed}/images/random`)
+                        .then((imgRespone) => imgRespone.json())
+                        .then((imgMessage) => {
+                            if (document.querySelector('img')) {
+                                let dogImage = document.querySelector('img');
+                                console.log('True');                               
+                                dogImage.src = imgMessage.message;
+                                dogImage.alt = `A ${breed} image`;
+                                bodyElement.appendChild(dogImage);
+                            } else {
+                                let dogImage = document.createElement('img');
+                                console.log('False');                                
+                                dogImage.src = imgMessage.message;
+                                dogImage.alt = `A ${breed} image`;
+                                bodyElement.appendChild(dogImage);
+                            }
+                        });
+                })
+            })
+        }
 
 
-fetch('https://dog.ceo/api/breeds/list/all')
-    .then((breedListResponse) => breedListResponse.json())
-    .then((breedMessage) => { 
-    const breedsmessage = breedMessage.message;
-    const breedslist = []
-    for (const [breed,variants] of Object.entries(breedsmessage)) {            
-        let dog = document.createElement("li");
-        
-        if (variants.length === 0) {
-            breedslist.push(breed);
-        }; 
+const buttonElement = document.querySelector('.js-button-breed');
 
-        for (const variant of variants) {
-            breedslist.push(`${variant} ${breed}`);
-        };
-      
-    };
-
-    const numberOfBreeds = breedslist.length;
-
-    
-
+buttonElement.addEventListener('click', (event) => {
+    event.preventDefault();
+    getRandomBreedAndImage();
 })
