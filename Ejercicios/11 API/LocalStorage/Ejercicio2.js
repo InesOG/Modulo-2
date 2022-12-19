9082259d7ee7a9d0ac4a29641b153f4a05438905
InesOG <i.ontillera@gmail.com>
2022-12-19 12:08:44 +0000

    Select background theme and save in localStorage

---
+++ Ejercicios/11 API/LocalStorage/Ejercicio2.js
@@ -0,0 +1,31 @@
+const inputlightEl = document.querySelector('#light');
+const inputdarkEl = document.querySelector('#dark');
+const bodyEl = document.querySelector('body');
+
+const preSavedTheme = localStorage.getItem("theme");
+
+if (preSavedTheme) { 
+    changeTheme(preSavedTheme);
+    if (preSavedTheme === 'light') { 
+        inputlightEl.setAttribute("checked", true); 
+    } else if (preSavedTheme === 'dark') {
+        inputdarkEl.setAttribute("checked", true);
+    }
+};
+
+function changeTheme(theme) {
+    bodyEl.setAttribute("class", `${theme}-background`);
+    saveSelection(theme);
+}
+
+function saveSelection(theme) {
+    localStorage.setItem("theme", theme);
+}
+
+inputdarkEl.addEventListener('click', () => {
+    changeTheme('dark');
+});
+
+inputlightEl.addEventListener('click', () => {
+    changeTheme('light');
+});
