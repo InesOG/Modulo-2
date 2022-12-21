81a0b01634f83a674999ef71a8ac06b363d96d73
InesOG <i.ontillera@gmail.com>
2022-12-21 22:06:37 +0000

    Added catching server errors

--- Ejercicios/11 API/Ejercicio2-2.js
+++ Ejercicios/11 API/Ejercicio2-2.js
@@ -7,7 +7,13 @@ fetch('https://api.thecatapi.com/v1/images/search?format=json', {
     'x-api-key': 'live_CMJdhWNKsORPnSDMmGEq9uL2FBuCzQ64QB3YAF9rvsgy6hRTNRnHgHC6BCjwHD9r',
   }
 })
-  .then(response => response.json())
+  .then(response => {
+    if (!response.ok) {
+      throw response;
+    }
+    return response.json();
+  })
   .then(data => {
     imgElement.setAttribute('src', data[0].url);
-  });
+  })
+  .catch((err) => console.log('error', err));
