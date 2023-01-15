e5a14bcb9ff8003569f481a4da532f1305b97955
InesOG <i.ontillera@gmail.com>
2023-01-15 21:37:43 +0000

    Acceder al parentElement y sus atributos

--- Ejercicios/13 DOM Avanzado/Ejemplo1.js
+++ Ejercicios/13 DOM Avanzado/Ejemplo1.js
@@ -5,3 +5,17 @@ const item1 = document.querySelector(`.item--1`);
 //  En Chrome se comportan igual, pero en Firefox por ejemplo:
 console.log(item1); // Devuelve una representación del elemento como HTML
 console.dir(item1); // Devuelve una representación del elemento como objeto
+
+
+const mother = item1.parentElement;
+
+console.log(mother); // devuelve la lista de atributos del ese elemento del DOM
+
+// Se puede acceder a cualquiera de los attributos. Ejemplo:
+
+console.log(
+  `La madre de nuestro elemento es un <${mother.nodeName.toLowerCase()}> y tiene la clase
+   ${mother.className}`
+);
+
+// Devuelve "La madre de nuestro elemento es un <ul> y tiene la clase .items"
